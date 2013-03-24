fa568fabcbd143c3b3a4613a5aca4101d0af11ac
Raymond Zeng <raymondzeng@live.com>
2013-03-24 01:47:41 -0400

    finished template for home page of website

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+
+app = Flask(__name__)
+global _name
+
+@app.route("/", methods = ["GET", "POST"])
+def home():
+    if request.method == "GET":
+        return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=5000)
