c0355315367b01dc21130d78c77cdcd13a94b129
Brian Lam <iBriaan@gmail.com>
2013-04-05 00:16:40 -0400

    Code cleanup
    Removed unnecessary imports and deleted commented out test code

--- inSync/src/com/example/insync/CreateSession.java
+++ inSync/src/com/example/insync/CreateSession.java
@@ -1,22 +1,13 @@
 package com.example.insync;
 
-import java.io.File;
-import java.io.FilenameFilter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.bluetooth.*;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.AlertDialog.Builder;
-import android.app.Dialog;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -66,60 +57,6 @@ public class CreateSession extends Activity {
 		getMenuInflater().inflate(R.menu.create_session, menu);
 		return true;
 	}
-
-	/*
-	//Choose an mp3 file - Code starts here
-	private String[] mFileList;
-	private File mPath = new File(Environment.getExternalStorageDirectory() + "//music//");
-	private String mChosenFile;
-	private static final String FTYPE = ".mp3";    
-	private static final int DIALOG_LOAD_FILE = 1000;
-
-	private void loadFileList(View v) {
-		try {
-			mPath.mkdirs();
-		}
-		catch(SecurityException e) {
-			//Log.e(TAG, "unable to write on the sd card " + e.toString());
-		}
-		if(mPath.exists()) {
-			FilenameFilter filter = new FilenameFilter() {
-				public boolean accept(File dir, String filename) {
-					File sel = new File(dir, filename);
-					return filename.contains(FTYPE) || sel.isDirectory();
-				}
-			};
-			mFileList = mPath.list(filter);
-		}
-		else {
-			mFileList= new String[0];
-		}
-	}
-
-	protected Dialog onCreateDialog(int id) {
-		Dialog dialog = null;
-		AlertDialog.Builder builder = new Builder(this);
-
-		switch(id) {
-		case DIALOG_LOAD_FILE:
-			builder.setTitle("Choose your file");
-			if(mFileList == null) {
-				//Log.e(TAG, "Showing file picker before loading the file list");
-				dialog = builder.create();
-				return dialog;
-			}
-			builder.setItems(mFileList, new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int which) {
-					mChosenFile = mFileList[which];
-					//you can do stuff with the file here too
-				}
-			});
-			break;
-		}
-		dialog = builder.show();
-		return dialog;
-	}
-	*/
 
 	public void enableBluetooth(){
 		BluetoothAdapter bA = BluetoothAdapter.getDefaultAdapter();
