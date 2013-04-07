85c2c5ea4e8f21588fd40b1bb7f62bf8399e4b8a
Brian Lam <iBriaan@gmail.com>
2013-04-06 22:14:59 -0400

    Code Cleanup and Commenting

--- inSync/src/com/example/insync/CreateSession.java
+++ inSync/src/com/example/insync/CreateSession.java
@@ -22,45 +22,50 @@ public class CreateSession extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_session);
-		getWindow().setSoftInputMode(
-			    WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 		
+		//Prevents on-screen keyboard from popping up when Activity is started
+		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
 		enableBluetooth();
-		
+
+		//Code for "Choose an mp3 file to live stream" button
 		final Button fCButton = (Button) findViewById(R.id.chooseFileButton);
-		
 		fCButton.setOnClickListener(new OnClickListener(){
 			public void onClick(View v){
 				try{
-				 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-	             intent.setType("file/*");
-	             final int PICKFILE_RESULT_CODE = 1;
-	             startActivityForResult(intent,PICKFILE_RESULT_CODE);
-			}
+					Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+					intent.setType("file/*");
+					final int PICKFILE_RESULT_CODE = 1;
+					startActivityForResult(intent,PICKFILE_RESULT_CODE);
+				}
+				
+				//Activity Not Found Crash fix
+				//Updates TextView with message to install a file browser
 				catch(Exception e){
 					final TextView fnTV = (TextView) findViewById(R.id.fileNameTextView);
 					fnTV.setText("Error: No File Browser found! Please install a file browser (Such as ASTRO File Manager) to browse for an MP3 file.");
 					fnTV.setTextColor(Color.RED);
 				}
-		}
+			}
 		});
 	}
-	
-	 @Override
-	 protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-	  // TODO Auto-generated method stub
-		 final int PICKFILE_RESULT_CODE = 1;
-	  switch(requestCode){
-	  case PICKFILE_RESULT_CODE:
-	   if(resultCode==RESULT_OK){
-	    String FilePath = data.getData().getPath();
-	    final TextView textFile = (TextView) findViewById(R.id.fileNameTextView); 
-	    textFile.setText("MP3 File Selected: " + FilePath);
-	   }
-	   break;
-	   
-	  }
-	 }
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		//Called after File Browser Activity returns file
+		final int PICKFILE_RESULT_CODE = 1;
+		switch(requestCode){
+		case PICKFILE_RESULT_CODE:
+			if(resultCode==RESULT_OK){
+				//Retrieve URI and display it in the TextView
+				String FilePath = data.getData().getPath();
+				final TextView textFile = (TextView) findViewById(R.id.fileNameTextView); 
+				textFile.setText("MP3 File Selected: " + FilePath);
+			}
+			break;
+
+		}
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
