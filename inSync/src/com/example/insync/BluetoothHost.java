bfa43fd5efdf9b7c28874500079fb3f1ca6a2c6f
Brian Lam <iBriaan@gmail.com>
2013-04-18 22:40:27 -0400

    Code cleanup and optimization

--- inSync/src/com/example/insync/BluetoothHost.java
+++ inSync/src/com/example/insync/BluetoothHost.java
@@ -1,52 +1,46 @@
 package com.example.insync;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-
 import android.os.Bundle;
+import android.net.Uri;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.content.ContentValues;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 
 public class BluetoothHost extends Activity {
-	
-	//Global String variable for filepath
-	String fp = "";
-	
-	//Global bluetooth adapter so that you don't have to keep calling a new Bluetooth adapter
-	BluetoothAdapter bA = BluetoothAdapter.getDefaultAdapter();
-	Set<BluetoothDevice> pairedDevices = bA.getBondedDevices();
+	private File fp;
+	private BluetoothAdapter bA = BluetoothAdapter.getDefaultAdapter();
+	private Set<BluetoothDevice> pairedDevices = bA.getBondedDevices();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_bluetooth_host);
 
-		//Get the filepath for the selected music file
 		Bundle extras = getIntent().getExtras();
-		fp = extras.getString("filepath");
+		fp = new File(extras.getString("filepath"));
 
-		//Display the filepath on the Textview
 		final TextView uriTV = (TextView) findViewById(R.id.uriDisplayTV);
 		uriTV.setText("Your selected song: " + fp);
 
- 		//This is for the "Send mp3 file" button
 		final Button sendbutton = (Button) findViewById(R.id.sendfilebutton);
 		sendbutton.setOnClickListener(new OnClickListener(){
 			public void onClick(View v){
-				//Call the sendFile() function which will use Bluetooth
 				sendFile();
 			}
 		});
 
-		//List the addresses for the connected Bluetooth devices
 		listConnectedDevices();
 
 	}
@@ -59,35 +53,51 @@ public class BluetoothHost extends Activity {
 	}
 
 	public void listConnectedDevices(){
-		//Get a list of the addresses of connected Bluetooth devices
 		List<String> s = new ArrayList<String>();
 		for(BluetoothDevice bt : pairedDevices){
-			
 			s.add(bt.getAddress());
 		}
 
-		//Get the TextView
 		final TextView btDevAddTV = (TextView) findViewById(R.id.connectedBTdevTV);
-		//Append the addresses for the Bluetooth devices onto the TextView
 		btDevAddTV.append("\n"+s.toString());
 	}
 
 	public void sendFile(){
+		if (bA == null) {
+			// Device does not support Bluetooth
+				return;
+			}
+		
+		// Bring up Android's Bluetooth Device chooser
+		Intent intent = new Intent();
+		intent.setAction(Intent.ACTION_SEND);
+		intent.setType("text/plain");
+		intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(fp) );
+		//...	
+		
+		//list of apps that can handle our intent
+		PackageManager pm = getPackageManager();
+		List<ResolveInfo> appsList = pm.queryIntentActivities( intent, 0);
 
-		//For loop:
-		//For each bluetooth device that is connected, send the music file
-		for(BluetoothDevice bt : pairedDevices){
-			ContentValues values = new ContentValues();
-			values.put(BluetoothShare.URI, "content://" + fp);
-
-			//Send Bluetooth stuff for each Address
-			values.put(BluetoothShare.DESTINATION, bt.getAddress());
-			values.put(BluetoothShare.DIRECTION, BluetoothShare.DIRECTION_OUTBOUND);
-			Long ts = System.currentTimeMillis();
-			values.put(BluetoothShare.TIMESTAMP, ts);
+		if(appsList.size() > 0) {
+			// proceed
+			//select bluetooth
+			String packageName = null;
+			String className = null;
+			
+			for(ResolveInfo info: appsList){
+			  packageName = info.activityInfo.packageName;
+			  if( packageName.equals("com.android.bluetooth")){
+			     className = info.activityInfo.name;
+			     break;// found
+			  }
+			}
+			
+			//set our intent to launch Bluetooth
+			intent.setClassName(packageName, className);
+			startActivity(intent);
 		}
-
-
+		
 	}
 
 }
