fb20b342006f7f39c2d2f510c7e1f58d3fcf00e8
Raymond Zeng <raymondzeng@live.com>
2013-04-25 01:23:57 -0400

    changing time in audio using slider is in sync

--- temp-web-stuff/our-app/app.py
+++ temp-web-stuff/our-app/app.py
@@ -1,6 +1,7 @@
 import os
 import werkzeug.serving
-from flask import Flask, request, render_template
+from werkzeug import secure_filename
+from flask import Flask, request, render_template, send_from_directory
 from gevent import monkey; monkey.patch_all()
 from socketio import socketio_manage
 from socketio.namespace import BaseNamespace
@@ -9,6 +10,9 @@ from socketio.server import SocketIOServer
 from cmixin import CustomMixin
 
 app = Flask(__name__)
+UPLOAD_FOLDER = './static/uploads'
+ALLOWED_EXTENSIONS = set(['mp3'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # The socket.io namespace
 class ChatNamespace(BaseNamespace, RoomsMixin, BroadcastMixin, CustomMixin):
@@ -25,23 +29,53 @@ class ChatNamespace(BaseNamespace, RoomsMixin, BroadcastMixin, CustomMixin):
         print room
         self.emit_to_room_and_you(room, 'playpause')
 
-    def on_skip_seven(self):
-        self.broadcast_event('skip')
+    def on_skip_seven(self, room):
+        self.emit_to_room_and_you(room, 'skip')
 
-    def on_reset(self):
-        self.broadcast_event('reset')
+    def on_reset(self, room):
+        self.emit_to_room_and_you(room, 'reset')
+
+    def on_time_changed(self, room, time):
+        self.emit_to_room(room, 'updateTime', time)
+        
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+def list_files():
+    folder = './static/uploads/'
+    print os.listdir(folder)
         
 # Flask routes
-@app.route('/')
+@app.route('/', methods = ['GET','POST'])
 def index():
     return render_template('chat.html')
 
+@app.route('/upload.html', methods = ['GET','POST'])
+def upload():
+    if request.method == 'POST':
+        print 'post'
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print 'successfully uploaded'
+            return render_template('upload.html')
+    if request.method == 'GET':
+        return render_template('upload.html')
+        
 @app.route("/socket.io/<path:path>")
 def run_socketio(path):
     socketio_manage(request.environ, {'': ChatNamespace})
-    
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
 @werkzeug.serving.run_with_reloader
 def run_dev_server():
+    print " * Running on http://127.0.0.1:5000/"
     app.debug = True
     port = 5000
     SocketIOServer(('', port), app, resource="socket.io").serve_forever()
